0670a2ec3e6c931b25565deaada42386110a21a2
david <f312213213david@gmail.com>
2023-06-01 15:37:26 +0800

    feat: update auto complete script

--- course-form-autocomplete/playwright.config.ts
+++ course-form-autocomplete/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://cof.ntpu.edu.tw/student_new.htm',
+    baseURL: 'https://cof.ntpu.edu.tw/',
 
     video: 'off',
   },
